8bef5523008c2619547224fc8d317d24f91793de
thang23092004 <85283565+thang23092004@users.noreply.github.com>
2021-06-03 16:15:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,4 +15,4 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'httphttps://www.facebook.com/thang.phamtrung.1422'
+    messLink: 'https://www.facebook.com/thang.phamtrung.1422'
